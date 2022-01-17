dfa4fbaad0b55271cf99b57943eb88af71b98e91
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-01-17 17:20:08 -0500

    able to create tables and post data in them

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,42 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+//O2M
+Category.hasMany(Product,{
+  foreignKey:"category_id"
+})
+//O2O
+Product.belongsTo(Category,{
+  foreignKey:"category_id"
+})
+//M2M
+Product.belongsToMany(Tag,{
+  through:ProductTag,
+  as: "",
+  foreignKey:'product_id'
+})
+//M2M
+Tag.belongsToMany(Product,{
+  through:ProductTag,
+  as:'',
+  foreignKey:'tag_id'
+})
+//O2O
+ProductTag.belongsTo(Product,{
+  foreignKey:'product_id'
+})
+//O2M
+Product.hasMany(ProductTag,{
+  foreignKey:'product_id'
+})
+//O2O
+ProductTag.belongsTo(Tag,{
+  foreignKey:'tag_id'
+})
+//O2M
+Tag.hasMany(ProductTag,{
+  foreignKey:'tag_id'
+})
 
 module.exports = {
   Product,
